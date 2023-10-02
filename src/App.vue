56229d38049c4b2f3c92828d1b11b92a9de1ad2b
magalutfullaev <mhoja9494@gmail.com>
2023-10-02 11:54:20 +0500

    add: Main & About

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
+  <Header/>
   <Main />
+  <About />
+  <Projects/>
+  <Contacts/>
 </template>
 
 <script>
-import Main from '@/views/Main.vue'
+import Main from '@/components/Main.vue'
+import Header from '@/components/Header.vue'
+import Contacts from '@/components/Contacts.vue'
+import Projects from '@/components/Projects.vue'
+import About from '@/components/About.vue'
 
 export default {
   name: 'App',
   components: {
-    Main
+    Main,
+    Header,
+    About,
+    Contacts,
+    Projects
   }
 }
 </script>
---
+++ src/components/About.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="container" id="about">
+    <div class="about-inner">
+      <div class="about-content">
+        <p class="section-name">/about</p>
+        <h2 class="title">This is now and past is passed</h2>
+        <p class="text about-text">Фронтенд-разработчик с опытом 3 года. Начал интересоваться программированием с 12 лет. Самоучка, всегда ищу новые знания. Люблю копаться на чем не знаю) Готов присоединиться к команде и внести свой вклад в современные веб-приложения. Работал с React.js 2+ года, Html/Css/Js 1+ года.</p>
+      </div>
+      <div class="bg-img about-img"></div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+#about {
+  padding-top: 100px;
+  padding-bottom: 100px;
+
+  .about-inner {
+    display: flex;
+    gap: 50px;
+
+    .about-content {
+      & > h2 {
+      }
+      .about-text {
+        padding-top: 20px;
+      }
+    }
+    .about-img {
+      flex: 0 0 450px;
+      border-radius: 30px;
+      width: 450px;
+      height: 300px;
+      background-image: url('@/assets/img/4.jpg');
+    }
+  }
+}
+</style>
+<script setup>
+</script>
